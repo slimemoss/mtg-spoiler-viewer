aaa86e7e25d27326acad414dbcf4b0e27916fec3
slimemoss <common.tken@gmail.com>
2023-08-29 08:01:50 +0900

    twitterカードのテスト

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Route, Routes, HashRouter } from 'react-router-dom'
+import { Helmet } from "react-helmet"
+
 import {Page} from './pages'
 import DataLTR from './data/LTR.json'
 import DataWOE from './data/WOE.json'
@@ -11,6 +13,16 @@ const root = createRoot(container)
 
 root.render(
   <>
+    <Helmet
+      title={'エルドレインの森 カードギャラリー | slimemoss'}
+      meta={[
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { property: 'og:image', content: 'https://raw.githubusercontent.com/slimemoss/mtg-spoiler-viewer/master/src/image/woe.png' },
+        { property: 'og:title', content: 'エルドレインの森 カードギャラリー' },
+        { property: 'og:description', content: '色とレアのフィルターができる、シンプルなカードギャラリーです。' }
+      ]}
+    />
+
     <div hidden={true}>
       <a href="#/ltr">ltr</a>
     </div>
