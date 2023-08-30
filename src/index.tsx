0aec72d025dad29b5794aa101e9bc9fd2f55398f
slimemoss <common.tken@gmail.com>
2023-08-31 08:47:39 +0900

    WOT追加

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { Helmet } from "react-helmet"
 import {Page} from './pages'
 import DataLTR from './data/LTR.json'
 import DataWOE from './data/WOE.json'
+import DataWOT from './data/WOT.json'
 
 const container = document.getElementById('app')
 if (!container) throw new Error('Failed to find the root element')
@@ -21,11 +22,23 @@ root.render(
       <a href="#/ltr">ltr</a>
     </div>
 
+    <div>
+      <ul>
+        <li>
+          <a href="#/woe">エルドレインの森</a>
+        </li>
+        <li>
+          <a href="#/wot">エルドレインの森 おとぎ話</a>
+        </li>
+      </ul>
+    </div>
+
     <HashRouter>
       <Routes>
         <Route path="/" element={<Page data={DataLTR} />}/>
         <Route path="/ltr" element={<Page data={DataLTR} />}/>
         <Route path="/woe" element={<Page data={DataWOE} />}/>
+        <Route path="/wot" element={<Page data={DataWOT} />}/>
       </Routes>
     </HashRouter>
   </>,
