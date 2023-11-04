09dcc1197d1ea95fb543f535a72157d457e2c532
slimemoss <common.tken@gmail.com>
2023-11-04 09:33:12 +0900

    ポリシーを明記

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,6 +34,10 @@ export const Page = (props: Props) => {
           <Card card={card}/>
         </div>
       ))}
-    </div> 
+    </div>
+
+    <footer>
+      本サイトはファンコンテンツ・ポリシーに沿った非公式のファンコンテンツです。ウィザーズ社の認可/許諾は得ていません。題材の一部に、ウィザーズ・オブ・ザ・コースト社の財産を含んでいます。©Wizards of the Coast LLC.
+    </footer>
   </>)
 }
