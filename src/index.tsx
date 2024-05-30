29c2887a76626a618969342492a95513694f8fd2
slimemoss <common.tken@gmail.com>
2024-05-30 13:22:54 +0900

    MH3対応

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { Route, Routes, HashRouter } from 'react-router-dom'
 import { Helmet } from "react-helmet"
 import {Page} from './pages'
-import OTJ from './data/OTJ.json'
-import BIG from './data/BIG.json'
+import MH3 from './data/MH3.json'
+import { Alert } from 'react-bootstrap'
 
 const container = document.getElementById('app')
 if (!container) throw new Error('Failed to find the root element')
@@ -16,18 +16,27 @@ root.render(
       title={'カードギャラリー | slimemoss'}
     />
 
-    <div>
+    <div hidden>
       <ul>
-        <li><a href="#/">サンダー・ジャンクションの無法者 (OTJ)</a></li>
-        <li><a href="#/big">サンダー・ジャンクションの無法者 ビッグスコア (BIG)</a></li>
+        <li><a href="#/">モダンホライゾン３</a></li>
       </ul>
     </div>
 
+    <Alert variant='warning'>
+      プレイブースターのうち、MH3のカードのみを扱っています。
+      <a href='https://magic.wizards.com/ja/products/modern-horizons-3/card-image-gallery'
+         target="_blank" rel="noopener noreferrer">スペシャルゲスト・統率者カードは公式ページをご覧ください。</a>
+    </Alert>
+
     <HashRouter>
       <Routes>
-        <Route path="/big" element={<Page data={BIG} setName="サンダー・ジャンクションの無法者 ビッグスコア" />}/>
-        <Route path="*" element={<Page data={OTJ} setName="サンダー・ジャンクションの無法者" />}/>
+        <Route path="*" element={<Page data={MH3} setName="モダンホライゾン３" />}/>
       </Routes>
     </HashRouter>
+
+    <footer style={{marginTop: '5rem', padding: '1rem'}}>
+      本サイトはファンコンテンツ・ポリシーに沿った非公式のファンコンテンツです。ウィザーズ社の認可/許諾は得ていません。題材の一部に、ウィザーズ・オブ・ザ・コースト社の財産を含んでいます。©Wizards of the Coast LLC.
+    </footer>
+
   </div>,
 )
