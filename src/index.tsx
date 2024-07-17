3bfbb8f4260dd4db09cc62f89939807cb3c63522
slimemoss <common.tken@gmail.com>
2024-07-17 17:26:34 +0900

    BLB一部対応

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Route, Routes, HashRouter } from 'react-router-dom'
 import { Helmet } from "react-helmet"
 import {Page} from './pages'
 import MH3 from './data/MH3.json'
+import BLB from './data/BLB.json'
 import { Alert } from 'react-bootstrap'
 
 const container = document.getElementById('app')
@@ -11,32 +12,33 @@ if (!container) throw new Error('Failed to find the root element')
 const root = createRoot(container)
 
 root.render(
-  <div>
-    <Helmet
-      title={'カードギャラリー | slimemoss'}
-    />
+    <div>
+        <Helmet
+            title={'カードギャラリー | slimemoss'}
+        />
 
-    <div hidden>
-      <ul>
-        <li><a href="#/">モダンホライゾン３</a></li>
-      </ul>
-    </div>
+        <div hidden>
+            <ul>
+                <li><a href="#/">モダンホライゾン３</a></li>
+            </ul>
+        </div>
 
-    <Alert variant='warning'>
-      プレイブースターのうち、MH3のカードのみを扱っています。
-      <a href='https://magic.wizards.com/ja/products/modern-horizons-3/card-image-gallery'
-         target="_blank" rel="noopener noreferrer">スペシャルゲスト・統率者カードは公式ページをご覧ください。</a>
-    </Alert>
+        <Alert variant='warning'>
+            プレイブースターのうち、MH3のカードのみを扱っています。
+            <a href='https://magic.wizards.com/ja/products/modern-horizons-3/card-image-gallery'
+               target="_blank" rel="noopener noreferrer">スペシャルゲスト・統率者カードは公式ページをご覧ください。</a>
+        </Alert>
 
-    <HashRouter>
-      <Routes>
-        <Route path="*" element={<Page data={MH3} setName="モダンホライゾン３" />}/>
-      </Routes>
-    </HashRouter>
+        <HashRouter>
+            <Routes>
+                <Route path="/blb" element={<Page data={BLB} setName="ブルームバロウ" />}/>
+                <Route path="*" element={<Page data={MH3} setName="モダンホライゾン３" />}/>
+            </Routes>
+        </HashRouter>
 
-    <footer style={{marginTop: '5rem', padding: '1rem'}}>
-      本サイトはファンコンテンツ・ポリシーに沿った非公式のファンコンテンツです。ウィザーズ社の認可/許諾は得ていません。題材の一部に、ウィザーズ・オブ・ザ・コースト社の財産を含んでいます。©Wizards of the Coast LLC.
-    </footer>
+        <footer style={{marginTop: '5rem', padding: '1rem'}}>
+            本サイトはファンコンテンツ・ポリシーに沿った非公式のファンコンテンツです。ウィザーズ社の認可/許諾は得ていません。題材の一部に、ウィザーズ・オブ・ザ・コースト社の財産を含んでいます。©Wizards of the Coast LLC.
+        </footer>
 
-  </div>,
+    </div>,
 )
