8472bd118d331f5a39261e81c5f2161e9da324ba
slimemoss <common.tken@gmail.com>
2025-03-27 20:44:02 +0000

    公式が見やすくなったので、更新終了

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react'
 import { createRoot } from 'react-dom/client'
-import { Route, Routes, HashRouter } from 'react-router-dom'
 import { Helmet } from "react-helmet"
 import Alert from 'react-bootstrap/Alert'
-import {Page} from './pages'
-import DATA from './data/DFT.json'
 
 const container = document.getElementById('app')
 if (!container) throw new Error('Failed to find the root element')
@@ -14,33 +11,26 @@ root.render(
   <div>
     <Helmet
       title={'カードギャラリー | slimemoss'}
-    />
+  />
 
-    <div hidden>
-      <ul>
-        <li><a href="#/">タイトル</a></li>
-      </ul>
-    </div>
+  <div hidden>
+    <ul>
+      <li><a href="#/">タイトル</a></li>
+    </ul>
+  </div>
 
-    <Alert variant="warning" hidden={true}>
-      プレビュー期間のため、一部カードは未掲載です。
-    </Alert>
+  <Alert variant="warning" hidden={true}>
+    プレビュー期間のため、一部カードは未掲載です。
+  </Alert>
 
-    <Alert variant="info">
-      <a href="https://magic.wizards.com/ja/products/aetherdrift/card-image-gallery?cigset=SPG"
-         target="_blank" rel="noopener noreferrer">
-        スペシャルゲストはこちら</a>
-    </Alert>
+  <Alert variant="info">
+    <a href="https://magic.wizards.com/ja">
+      公式サイトのカードギャラリー
+    </a>
 
-    <HashRouter>
-      <Routes>
-        <Route path="*" element={<Page data={DATA} setName="霊気走破" />}/>
-      </Routes>
-    </HashRouter>
-
-    <footer style={{marginTop: '5rem', padding: '1rem'}}>
-      本サイトはファンコンテンツ・ポリシーに沿った非公式のファンコンテンツです。ウィザーズ社の認可/許諾は得ていません。題材の一部に、ウィザーズ・オブ・ザ・コースト社の財産を含んでいます。©Wizards of the Coast LLC.
-    </footer>
+    が見やすくなったため、本サービスは終了しました。
+    ご利用いただきありがとうございました。
+  </Alert>
 
   </div>,
 )
